6dfbe4c20a2131ab6988c4b3ccffb663243fe570
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2020-09-29 14:42:33 +0600

    feat: delete cards and open picture popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,5 @@
 const popup = document.querySelector('.popup');
+const popupImage = document.querySelector('.image-popup');
 
 let profileName = document.querySelector('.profile__name');
 let profileDescription = document.querySelector('.profile__description');
@@ -11,6 +12,7 @@ let closeButton = document.querySelector('.popup__close-button');
 let addButton = document.querySelector('.profile__add-button');
 let submitButton = document.querySelector('.popup__submit-button');
 let form = document.querySelector('.popup__card');
+let imageCloseButton = document.querySelector('.image-popup__close-button');
 
 const cardsTemplate = document.querySelector('.template').content;
 const grid = document.querySelector('.elements__grid');
@@ -50,13 +52,14 @@ function render() {
     setListeners();
 };
 
-function renderItem(card) {
+function renderItem(card, index) {
     const htmlElement = cardsTemplate.cloneNode(true);
     const link = card.link;
     const name = card.name;
     htmlElement.querySelector('.element__image').setAttribute('src', link);
     htmlElement.querySelector('.element__image').setAttribute('alt', name);
     htmlElement.querySelector('.element__title').textContent = name;
+    htmlElement.querySelector('.element').setAttribute('id', index)
     grid.appendChild(htmlElement);
 };
 
@@ -64,8 +67,15 @@ function setListeners() {
     editButton.addEventListener('click', handleEditInfo);
     closeButton.addEventListener('click', handlePopupToggle);
     addButton.addEventListener('click', handleAdd);
+    document.querySelectorAll('.element__image').forEach((card) => {
+        card.addEventListener('click', imagePopup)
+    })
+    imageCloseButton.addEventListener('click', handlePopupImageToggle);
     document.querySelectorAll('.element__button').forEach((button) => {
         button.addEventListener('click', handleLike);
+    });
+    document.querySelectorAll('.element__delete').forEach((button) => {
+        button.addEventListener('click', handleDelete);
     });
     form.addEventListener('submit', handleSubmit);
 };
@@ -76,9 +86,29 @@ let handlePopupToggle = function () {
     popupSecondLine.value = null;
 };
 
+let imagePopup = function (event) {
+    handlePopupImageToggle();
+    let id = event.target.parentNode.getAttribute('id');
+    let card = document.getElementById(id);
+    let link = card.querySelector('.element__image').getAttribute('src');
+    let name = card.querySelector('.element__title').textContent;
+    popupImage.querySelector('.image-popup__image').setAttribute('src', link);
+    popupImage.querySelector('.image-popup__title').textContent = name;
+}
+
+let handlePopupImageToggle = function () {
+    popupImage.classList.toggle('image-popup_active');
+};
+
 let handleLike = function (event) {
     event.target.classList.toggle('element__button_active');
 };
+
+let handleDelete = function(event) {
+    let id = event.target.parentNode.getAttribute('id');
+    let card = document.getElementById(id);
+    card.remove();
+}
 
 let handleEditInfo = function () {
     handlePopupToggle();
@@ -105,7 +135,13 @@ let addCard = function (name, link) {
     htmlElement.querySelector('.element__image').setAttribute('src', link);
     htmlElement.querySelector('.element__image').setAttribute('alt', name);
     htmlElement.querySelector('.element__title').textContent = name;
+    const id = document.querySelectorAll('.element').length;
+    htmlElement.querySelector('.element').setAttribute('id', id);
     htmlElement.querySelector('.element__button').addEventListener('click', handleLike);
+    htmlElement.querySelector('.element__delete').addEventListener('click', handleDelete);
+    htmlElement.querySelectorAll('.element__image').forEach((card) => {
+        card.addEventListener('click', imagePopup)
+    })
     grid.prepend(htmlElement);
 }
 
