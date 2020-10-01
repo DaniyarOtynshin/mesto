8bf8f7e3e2618765a2403c602309c00fadfab838
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2020-10-01 10:24:26 +0600

    refactor

--- scripts/index.js
+++ scripts/index.js
@@ -12,7 +12,7 @@ const popupFirstLine = popupAdd.querySelector('[name="title"]');
 const popupSecondLine = popupAdd.querySelector('[name="link"]');
 
 const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelectorAll('.popup__close-button');
+const closeButtons = document.querySelectorAll('.popup__close-button');
 const addButton = document.querySelector('.profile__add-button');
 const formEdit = document.querySelector('.popup__card');
 const formAdd = document.querySelector('.popup-add__card');
@@ -49,8 +49,6 @@ const initialCards = [
     }
 ];
 
-const state = {mode : 'edit'};
-
 const render = function () {
     initialCards.forEach((card) => {
         createCard(card.name, card.link);
@@ -67,9 +65,9 @@ const closePopup = function(event) {
 
 const handleImagePopup = function (event) {
     openPopup(popupImage);
-    let card = event.target.parentNode;
-    let link = card.querySelector('.element__image').getAttribute('src');
-    let name = card.querySelector('.element__title').textContent;
+    const card = event.target.parentNode;
+    const link = card.querySelector('.element__image').getAttribute('src');
+    const name = card.querySelector('.element__title').textContent;
     popupImage.querySelector('.popup-image__image').setAttribute('src', link);
     popupImage.querySelector('.popup-image__title').textContent = name;
 }
@@ -79,13 +77,12 @@ const handleLike = function (event) {
 };
 
 const handleDelete = function(event) {
-    let card = event.target.parentNode;
+    const card = event.target.parentNode;
     card.remove();
 }
 
 const handleEditInfo = function () {
     openPopup(popupEdit);
-    state.mode = 'edit';
     if (popupEdit.classList.contains('popup_active')) {
         popupName.value = profileName.textContent
         popupDescription.value = profileDescription.textContent
@@ -94,7 +91,6 @@ const handleEditInfo = function () {
 
 const handleAddPopup = function () {
     openPopup(popupAdd);
-    state.mode = 'add';
 };
 
 const createCard = function (name, link) {
@@ -113,33 +109,29 @@ const addCard = function (newCard) {
     grid.prepend(newCard);
 }
 
-const handleSubmit = function (event) {
-    if (state.mode === 'edit') {
-        event.preventDefault();
-        profileName.textContent = popupName.value;
-        profileDescription.textContent = popupDescription.value;
-        closePopup(event);
-    }
-    else if (state.mode === 'add') {
-        event.preventDefault();
-        const popupTitle = popupFirstLine.value;
-        const popupLink = popupSecondLine.value;
-        createCard(popupTitle, popupLink);
-        closePopup(event);
-        popupFirstLine.value = null;
-        popupSecondLine.value = null;
-    }
+const handleEditSubmit = function (event) {
+    event.preventDefault();
+    profileName.textContent = popupName.value;
+    profileDescription.textContent = popupDescription.value;
+    closePopup(event);
 };
+
+const handleAddSubmit = function (event) {
+    event.preventDefault();
+    const popupTitle = popupFirstLine.value;
+    const popupLink = popupSecondLine.value;
+    createCard(popupTitle, popupLink);
+    closePopup(event);
+    popupFirstLine.value = null;
+    popupSecondLine.value = null;
+}
 
 editButton.addEventListener('click', handleEditInfo);
 addButton.addEventListener('click', handleAddPopup);
-closeButton.forEach((btn) => {
+closeButtons.forEach((btn) => {
     btn.addEventListener('click', closePopup);
 });
-document.querySelectorAll('.element__image').forEach((card) => {
-    card.addEventListener('click', handleImagePopup)
-});
-formEdit.addEventListener('submit', handleSubmit);
-formAdd.addEventListener('submit', handleSubmit);
+formEdit.addEventListener('submit', handleEditSubmit);
+formAdd.addEventListener('submit', handleAddSubmit);
 
 render();
