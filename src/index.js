b5bc7de2ab0766bba3c07d176734566b09580dc8
Daniyar Otynshin <daniyar.otynshin@yandex.kz>
2020-12-01 17:17:07 +0600

    change file structure

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import Card from "../components/Card.js";
-import FormValidator from "../components/FormValidator.js";
-import PopupWithImage from "../components/PopupWithImage.js";
-import PopupWithForm from "../components/PopupWithForm.js";
-import Section from "../components/Section.js";
-import UserInfo from "../components/UserInfo.js";
+import Card from "./components/Card.js";
+import FormValidator from "./components/FormValidator.js";
+import PopupWithImage from "./components/PopupWithImage.js";
+import PopupWithForm from "./components/PopupWithForm.js";
+import Section from "./components/Section.js";
+import UserInfo from "./components/UserInfo.js";
 import {
     editButton,
     addButton,
@@ -12,7 +12,7 @@ import {
     formEdit,
     containerSelector,
     popupImageSelector
-} from "../scripts/constants.js";
+} from "./scripts/constants.js";
 import "./pages/index.css";
 
 const formParameters = {
